6e4d693260373e92701e77d1e095693e20458dc5
adach-203844 <szymona96@gmail.com>
2018-03-12 09:11:58 +0100

    Code style file deleted and added to .gitignore, StackExerciseTest initialized

--- src/test/java/pl/ttpsc/testing/junit/stack/StackExerciseTest.java
+++ src/test/java/pl/ttpsc/testing/junit/stack/StackExerciseTest.java
@@ -1,5 +1,36 @@
 package pl.ttpsc.testing.junit.stack;
 
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 public class StackExerciseTest {
+
+    public static long startTime;
+
+    StackExercise sut;
+
+    @BeforeClass
+    public static void beforeClass() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @Before
+    public void setUp() {
+        sut = new StackExercise();
+    }
+
+    
+
+    @After
+    public void tearDown() {
+        sut = null;
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Total time " + (System.currentTimeMillis() - startTime) + " ms.");
+    }
 }

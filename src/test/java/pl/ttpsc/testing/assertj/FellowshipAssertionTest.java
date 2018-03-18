879495f0d2043debd84f119fb6afdb8d1f557755
adach-203844 <szymona96@gmail.com>
2018-03-18 15:37:27 +0100

    AssertJ fellowship tests completed

--- src/test/java/pl/ttpsc/testing/assertj/FellowshipAssertionTest.java
+++ src/test/java/pl/ttpsc/testing/assertj/FellowshipAssertionTest.java
@@ -1,5 +1,6 @@
 package pl.ttpsc.testing.assertj;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,34 +22,45 @@ public class FellowshipAssertionTest {
 
     @Test
     public void shouldFrodoNameBeCorrectAndNotBeASauronAndBeInFellowship() {
+        assertThat(fellowship).extracting(Fellow::getName).contains("Frodo").doesNotContain("Sauron");
     }
 
     @Test
     public void aragornShouldBeBeforeBoromir() {
+        assertThat(fellowship).containsSubsequence(aragorn(), boromir());
     }
 
     @Test
     public void shouldNotContainDuplicatedFellows() {
+        assertThat(fellowship).doesNotHaveDuplicates();
     }
 
     @Test
     public void shouldContainOnlyFellowsWithExpectedNamesInProperOrder() {
+        assertThat(fellowship).extracting(Fellow::getName)
+                .containsSequence("Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Legolas", "Gimli", "Aragorn", "Boromir");
     }
 
     @Test
     public void shouldContainNineFellowsButNoneGiant() {
-        assertThat(fellowship).hasSize(9);
+        assertThat(fellowship).hasSize(9).filteredOn(f -> f.getRace().equals(Fellow.Race.GIANT)).hasSize(0);
     }
 
     @Test
     public void shouldContainTupleForBoromirSamAndLegolas() {
         // Extracting multiple values at once (using tuple to group them)
         // Create tuples with name and race name
+        assertThat(fellowship).extracting(Fellow::getName, Fellow::getRace)
+                .contains(
+                        Tuple.tuple("Boromir", Fellow.Race.MAN),
+                        Tuple.tuple("Sam", Fellow.Race.HOBBIT),
+                        Tuple.tuple("Legolas", Fellow.Race.ELF));
     }
 
 
     @Test
     public void shouldContainsFourHobbits() {
+        assertThat(fellowship).filteredOn(f -> f.getRace().equals(Fellow.Race.HOBBIT)).hasSize(4);
     }
 
 
